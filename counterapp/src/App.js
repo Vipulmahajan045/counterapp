a2b5eb39d427bec791fcf1165ca985eb0bfec12e
Vipulmahajan045 <vipulmahajan170140111045@gmail.com>
2021-08-24 12:14:31 +0530

    done

---
+++ counterapp/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import CC from './CC';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{marginTop:"100px"}}>COUNTER APPLICATION</h1>
+      <CC/>
+    </div>
+  );
+}
+
+export default App;
